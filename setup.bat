fb1ff061d763540ad54ccff5db9cfcf3b55af185
jujubaaaaa <julia.pereira.am84@gmail.com>
2024-04-11 11:32:38 -0300

    setup feito e testado

---
+++ setup.bat
@@ -0,0 +1,9 @@
+git config --global user.name "jujubaaaaa"
+git config --global user.email "julia.pereira.am84@gmail.com"
+
+cd C:\Users\JULIAPEREIRADEALMEID
+
+git clone https://github.com/jujubaaaaa/Caesar-Cipher.git
+cd Caesar-Cipher
+
+code .
